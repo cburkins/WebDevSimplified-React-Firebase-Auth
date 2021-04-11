cd77453fb80678b76a46e9642c04b7d46eb9f60e
Chad Burkins <cburkin@its.jnj.com>
2021-04-11 08:38:24 -0400

    not working

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,48 @@
 import React from "react";
 import Signup from "./Signup";
-import { Container } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
+import LoginPage from "./LoginPage";
 import PrivateRoute from "./PrivateRoute";
 import ForgotPassword from "./ForgotPassword";
 import UpdateProfile from "./UpdateProfile";
 
 function App() {
     return (
-        <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
-            <div className="w-100" style={{ maxWidth: "400px" }}>
-                <Router>
-                    <AuthProvider>
-                        <Switch>
-                            <PrivateRoute exact path="/" component={Dashboard} />
-                            <PrivateRoute path="/update-profile" component={UpdateProfile} />
-                            <Route path="/signup" component={Signup} />
-                            <Route path="/login" component={Login} />
-                            <Route path="/forgot-password" component={ForgotPassword} />
-                        </Switch>
-                    </AuthProvider>
-                </Router>
-            </div>
-        </Container>
+        <div>
+            <Router>
+                <Navbar bg="light">
+                    {/* 1st Nav item with className mr-auto pushes the 2nd Nav item to the right */}
+                    <Nav className="mr-auto">
+                        <Nav.Link as={Link} to="/">
+                            Home
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/publicpage">
+                            PublicPage
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/privatepage01">
+                            PrivatePage01
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/privatepage02">
+                            PrivatePage02
+                        </Nav.Link>
+                    </Nav>
+                </Navbar>
+
+                <AuthProvider>
+                    <Switch>
+                        <PrivateRoute exact path="/" component={Dashboard} />
+                        <PrivateRoute path="/update-profile" component={UpdateProfile} />
+                        <Route path="/signup" component={Signup} />
+                        <Route path="/login" component={LoginPage} />
+                        <Route path="/forgot-password" component={ForgotPassword} />
+                    </Switch>
+                </AuthProvider>
+            </Router>
+        </div>
     );
 }
 
